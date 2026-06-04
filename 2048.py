539c4356501bc6ecf11a2ff8054983136a1e0e07
Daniel-Brenner <danielbrennerno.2@gmail.com>
2026-06-04 04:46:48 -0400

    completed gameplay, no score

--- 2048.py
+++ 2048.py
@@ -4,7 +4,7 @@ import random
 def onAppStart(app):
     app.bestScore = 0
     resetApp(app)
-
+ 
 def resetApp(app):
     app.boardLeft = (50/400) * app.width
     app.boardTop = (75/400) * app.height
@@ -16,148 +16,200 @@ def resetApp(app):
     addRandom(app)
     addRandom(app)
     app.curScore = 0
-
+    app.gameOver = False
+    app.gameWon = False
+ 
 def boardGen(app):
     res = []
     for _ in range(app.rows):
         newRow = [None] * app.cols
         res.append(newRow)
     return res
-
+ 
 def addRandom(app):
+    emptyCells = []
+    for row in range(app.rows):
+        for col in range(app.cols):
+            if app.boardStatus[row][col] == None:
+                emptyCells.append((row, col))
+    if emptyCells == []:
+        return
     randNum = random.randrange(101)
     if randNum >= 90:
         nextNum = 4
     else:
         nextNum = 2
-    added = False
-    while added == False:
-        randRow,randCol = random.randrange(app.rows),random.randrange(app.cols)
-        if app.boardStatus[randRow][randCol] == None:
-            app.boardStatus[randRow][randCol] = nextNum
-            added = True
-
+    randRow, randCol = random.choice(emptyCells)
+    app.boardStatus[randRow][randCol] = nextNum
+ 
 def redrawAll(app):
     drawBoard(app)
     drawScore(app)
-
+    if app.gameOver:
+        drawGameOver(app)
+    if app.gameWon:
+        drawWin(app)
+ 
 def drawScore(app):
-    drawLabel('Welcome to 2048!',app.width/2,(1/12)*app.height,size=14,bold=True)
-    drawLabel(f'Current Score = {app.curScore}',(3/16)*app.width,(9/64)*app.height)
-    drawLabel(f'Best Score = {app.bestScore}',(13/16)*app.width,(9/64)*app.height)
-
+    drawLabel('Welcome to 2048!', app.width/2, (1/12)*app.height, size=14, bold=True)
+    drawLabel(f'Current Score = {app.curScore}', (3/16)*app.width, (9/64)*app.height)
+    drawLabel(f'Best Score = {app.bestScore}', (13/16)*app.width, (9/64)*app.height)
+ 
+def drawGameOver(app):
+    drawRect(app.width/2, app.height/2, 300, 150,
+             fill='black', opacity=80, align='center')
+    drawLabel('Game Over!', app.width/2, app.height/2 - 20,
+              size=24, bold=True, fill='white')
+    drawLabel('Press R to Restart', app.width/2, app.height/2 + 20,
+              size=16, fill='white')
+ 
+def drawWin(app):
+    drawRect(app.width/2, app.height/2, 300, 150,
+             fill='gold', opacity=80, align='center')
+    drawLabel('You Reached 2048!', app.width/2, app.height/2 - 20,
+              size=20, bold=True, fill='white')
+    drawLabel('R to Restart or Keep Playing!', app.width/2, app.height/2 + 20,
+              size=14, fill='white')
+ 
 def drawBoard(app):
     drawGrid(app)
     drawOutline(app)
-
+ 
 def drawGrid(app):
     for row in range(app.rows):
         for col in range(app.cols):
-            drawCell(app,row,col)
-
-def drawCell(app,row,col):
-    cellWidth,cellHeight = getCellSize(app)
-    cellLeft,cellTop = getCellLeftTop(app,row,col,cellWidth,cellHeight)
-    cellMidX,cellMidY = getCellMid(cellLeft,cellTop,cellWidth,cellHeight)
+            drawCell(app, row, col)
+ 
+def drawCell(app, row, col):
+    cellWidth, cellHeight = getCellSize(app)
+    cellLeft, cellTop = getCellLeftTop(app, row, col, cellWidth, cellHeight)
+    cellMidX, cellMidY = getCellMid(cellLeft, cellTop, cellWidth, cellHeight)
     cellNum = app.boardStatus[row][col]
-    colors = {2:'yellow',
-              4:'orange',
-              8:'pink',
-              16:'blue',
-              32:'maroon',
-              64:'crimson',
-              128:'cyan',
-              256:'lightGreen',
-              512:'violet',
-              1024:'indigo',
-              2048:'brown'}
+    colors = {2: 'yellow',
+              4: 'orange',
+              8: 'pink',
+              16: 'blue',
+              32: 'maroon',
+              64: 'crimson',
+              128: 'cyan',
+              256: 'lightGreen',
+              512: 'violet',
+              1024: 'indigo',
+              2048: 'brown'}
     if cellNum in colors:
         curColor = colors[cellNum]
-        drawRect(cellLeft,cellTop,cellWidth,cellHeight,fill=curColor,border='black')
-        drawLabel(str(cellNum),cellMidX,cellMidY)
+        drawRect(cellLeft, cellTop, cellWidth, cellHeight, fill=curColor, border='black')
+        drawLabel(str(cellNum), cellMidX, cellMidY)
     if cellNum == None:
-        drawRect(cellLeft,cellTop,cellWidth,cellHeight,fill=None,border='black')
-
-def getCellLeftTop(app,row,col,cellWidth,cellHeight):
-    return app.boardLeft + col * cellWidth,app.boardTop + row * cellHeight
-
+        drawRect(cellLeft, cellTop, cellWidth, cellHeight, fill=None, border='black')
+ 
+def getCellLeftTop(app, row, col, cellWidth, cellHeight):
+    return app.boardLeft + col * cellWidth, app.boardTop + row * cellHeight
+ 
 def getCellSize(app):
-    return app.boardWidth / app.cols, app.boardHeight/app.rows
-    
+    return app.boardWidth / app.cols, app.boardHeight / app.rows
+ 
 def drawOutline(app):
-    drawRect(app.boardLeft,app.boardTop,app.boardWidth,app.boardHeight,fill=None,border='black',borderWidth=4)
-
-def getCellMid(cellLeft,cellTop,cellWidth,cellHeight):
+    drawRect(app.boardLeft, app.boardTop, app.boardWidth, app.boardHeight,
+             fill=None, border='black', borderWidth=4)
+ 
+def getCellMid(cellLeft, cellTop, cellWidth, cellHeight):
     return cellLeft + cellWidth / 2, cellTop + cellHeight / 2
-
-def onKeyPress(app,key):
-    if isLegal(app,key):
-        makeMove(app,key)
+ 
+def onKeyPress(app, key):
+    if key == 'r':
+        resetApp(app)
+        return
+    if app.gameOver:
+        return
+    if key in {'up', 'down', 'left', 'right'} and isLegal(app, key):
+        makeMove(app, key)
         addRandom(app)
-
-def isLegal(app,key):
-    if key not in {'up','down','left','right'}:
+        if not app.gameWon:
+            app.gameWon = checkWin(app)
+        if isGameOver(app):
+            app.gameOver = True
+ 
+def isLegal(app, key):
+    if key not in {'up', 'down', 'left', 'right'}:
         return False
     if key == 'up':
-        colsCheck = [False] * app.cols
         for col in range(app.cols):
-            colFound = False
-            colNum = None
-            for row in range(app.rows - 1,-1,-1):
-                if app.boardStatus[row][col] == None:
-                    colsCheck[col] = True
-                elif app.boardStatus[row][col] != None and not colFound:
-                    colFound = True
-                    colNum = app.boardStatus[row][col]
-                else: #app.boardStatus[row][col] != None and colFound
-                    if app.boardStatus[row][col] == colNum:
-                        colsCheck[col] = True
-        return colsCheck == ([True] * app.cols)
+            colList = [app.boardStatus[row][col] for row in range(app.rows)]
+            if shift(colList) != colList:
+                return True
     elif key == 'down':
-        pass
+        for col in range(app.cols):
+            colList = [app.boardStatus[row][col] for row in range(app.rows - 1, -1, -1)]
+            if shift(colList) != colList:
+                return True
     elif key == 'left':
-        pass
-    else: #key = 'right'
-        pass
-
-def makeMove(app,key):
+        for row in range(app.rows):
+            rowList = app.boardStatus[row][:]
+            if shift(rowList) != rowList:
+                return True
+    else:  # key == 'right'
+        for row in range(app.rows):
+            rowList = app.boardStatus[row][::-1]
+            if shift(rowList) != rowList:
+                return True
+    return False
+ 
+def makeMove(app, key):
     if key == 'up':
         allCols = []
         for col in range(app.cols):
-            newCol = []
+            colList = []
             for row in range(app.rows):
-                newCol.append(app.boardStatus[row][col])
-            allCols.append(newCol)
+                colList.append(app.boardStatus[row][col])
+            allCols.append(colList)
         temps = manageShift(allCols)
         app.boardStatus = switchRowsCol(temps)
-        # for col in range(app.cols):
-        #     colFound = False
-        #     for row in range(app.rows - 1,-1,-1):
-        #         if app.boardStatus[row][col] == None and not colFound:
-        #             continue
-        #         elif app.boardStatus[row][col] == None and colFound:
-        #             app.boardStatus[row][col] = colNum
-        #             app.boardStatus[storeRow][storeCol] = None
-        #             storeRow,storeCol = row,col
-        #         elif app.boardStatus[row][col] != None and not colFound:
-        #             colFound = True
-        #             colNum = app.boardStatus[row][col]
-        #             storeRow,storeCol = row, col
-        #         else: #app.boardStatus[row][col] != None and colFound:
-        #             if app.boardStatus[row][col] == colNum:
-        #                 app.boardStatus[row][col] = app.boardStatus[row][col] * 2
-        #                 app.boardStatus[storeRow][storeCol] = None
-        #             else:
-        #                 colNum = app.boardStatus[row][col]
-        #                 storeRow,storeCol = row,col
-
+    elif key == 'down':
+        allCols = []
+        for col in range(app.cols):
+            colList = []
+            for row in range(app.rows - 1, -1, -1):
+                colList.append(app.boardStatus[row][col])
+            allCols.append(colList)
+        temps = manageShift(allCols)
+        reversedTemps = []
+        for colList in temps:
+            reversedTemps.append(colList[::-1])
+        app.boardStatus = switchRowsCol(reversedTemps)
+    elif key == 'left':
+        app.boardStatus = manageShift(app.boardStatus)
+    else:  # key == 'right'
+        allRows = []
+        for row in range(app.rows):
+            rowList = app.boardStatus[row][::-1]
+            allRows.append(rowList)
+        temps = manageShift(allRows)
+        app.boardStatus = []
+        for rowList in temps:
+            app.boardStatus.append(rowList[::-1])
+ 
+def checkWin(app):
+    for row in range(app.rows):
+        for col in range(app.cols):
+            if app.boardStatus[row][col] == 2048:
+                return True
+    return False
+ 
+def isGameOver(app):
+    for key in ['up', 'down', 'left', 'right']:
+        if isLegal(app, key):
+            return False
+    return True
+ 
 def manageShift(L):
     res = []
     for entry in L:
         res.append(shift(entry))
     return res
-
+ 
 def shift(L):
+    targetLen = len(L)
     newL = removeWhiteSpace(L)
     res = []
     skip = False
@@ -173,22 +225,21 @@ def shift(L):
                 skip = True
     if not skip and len(newL) > 0:
         res.append(newL[-1])
-    return addWhiteSpace(res)
-
+    return addWhiteSpace(res, targetLen)
+ 
 def removeWhiteSpace(L):
     res = []
     for entry in L:
         if entry != None:
             res.append(entry)
     return res
-
-def addWhiteSpace(L):
-    res = L
-    gridLengthVariable = 4
-    while len(res) < gridLengthVariable:
+ 
+def addWhiteSpace(L, targetLen):
+    res = L[:]
+    while len(res) < targetLen:
         res.append(None)
     return res
-
+ 
 def switchRowsCol(L):
     res = []
     for col in range(len(L[0])):
@@ -197,8 +248,8 @@ def switchRowsCol(L):
             newCol.append(L[row][col])
         res.append(newCol)
     return res
-    
+ 
 def main():
-    runApp(width=500,height=500)
-
+    runApp(width=500, height=500)
+ 
 main()
