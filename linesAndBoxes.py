f0da659f17c259fb14b482194d678b6ecb1ec42f
Daniel-Brenner <danielbrennerno.2@gmail.com>
2026-06-15 07:35:22 -0400

    added instructions tab

--- linesAndBoxes.py
+++ linesAndBoxes.py
@@ -12,6 +12,7 @@ def resetApp(app):
     app.dim = 7
     app.colorSelectDone = False
     app.gridSizeSelectDone = False
+    app.instruction = False
 
 ################################################
 #__selectScreen___
@@ -41,6 +42,8 @@ def select_redrawAll(app):
             drawLabel('selected',(2/4)*app.width + adj,(3/4)*app.height,size=14,align='left')
     if app.colorSelectDone:
         drawSmallDots(app)
+    if app.instruction:
+        drawInstruction(app)
 
 def drawSmallDots(app):
     squareOffset = (30/400) * app.width
@@ -55,54 +58,85 @@ def drawSmallDots(app):
             y = squareTop + j * difference
             drawCircle(x,y,smallDotRadius)
 
-def select_onKeyPress(app,key):
-    if not app.colorSelectDone:
-        if not app.p1ColorSelected:
-            if key == 'left':
-                if app.colorSelectIndex == 0:
-                    app.colorSelectIndex = len(app.colors) - 1
-                else:
-                    app.colorSelectIndex -= 1
-            elif key == 'right':
-                if app.colorSelectIndex == len(app.colors) - 1:
-                    app.colorSelectIndex = 0
-                else:
-                    app.colorSelectIndex += 1
-            elif key == 'enter':
-                app.p1Color = app.colors[app.colorSelectIndex]
-                app.colors.pop(app.colorSelectIndex)
-                app.colorSelectIndex = 0
-                app.p1ColorSelected = True
+def drawInstruction(app):
+    allInstructions = ['Instructions for Lines and Boxes!',
+                       '',
+                       "press 'i' to exit instructions",
+                       'left and right arrow keys to select',
+                       'enter key to advance to next step',
+                       'move mouse over line that you want to place',
+                       'click to place the ghost piece',
+                       'score by placing the line that completes a box',
+                       'if you score, you get another turn, else opponent gets turn',
+                       'game is over when all boxes are filled',
+                       '']
+    rectWidth,rectHeight = (7/8) * app.width, (1/2)*app.height
+    drawRect(app.width/2,app.height/2,rectWidth,rectHeight,fill='purple',border='black',align='center')
+    boxTop = (app.height/2) - (1/2) * (rectHeight)
+    boxBot = (app.height/2) + (1/2) * (rectHeight)
+    boxHeight = boxBot - boxTop
+    lineDiv = boxHeight / len(allInstructions)
+    for i in range(1,len(allInstructions) + 1):
+        y = boxTop + i * lineDiv
+        if i == 1:
+            drawLabel(allInstructions[i-1],app.width/2,y,size=15,bold=True)
         else:
-            if key == 'left':
-                if app.colorSelectIndex == 0:
-                    app.colorSelectIndex = len(app.colors) - 1
-                else:
-                    app.colorSelectIndex -= 1
-            elif key == 'right':
-                if app.colorSelectIndex == len(app.colors) - 1:
-                    app.colorSelectIndex = 0
-                else:
-                    app.colorSelectIndex += 1
-            elif key == 'enter':
-                app.p2Color = app.colors[app.colorSelectIndex]
-                app.colorSelectDone = True
-    elif not app.gridSizeSelectDone:
-        if key == 'right':
-            if app.dim == 10:
-                app.dim = 4
-            else:
-                app.dim += 1
-        elif key == 'left':
-            if app.dim == 4:
-                app.dim = 10
-            else:
-                app.dim -= 1
-        elif key == 'enter':
-            app.gridSizeSelectDone = True
+            drawLabel(allInstructions[i-1],app.width/2,y)
+
+def select_onKeyPress(app,key):
+    if app.instruction:
+        if key == 'i':
+            app.instruction = not app.instruction
     else:
-        if key == 'enter':
-            setActiveScreen('game')
+        if key == 'i':
+            app.instruction = not app.instruction
+        if not app.colorSelectDone:
+            if not app.p1ColorSelected:
+                if key == 'left':
+                    if app.colorSelectIndex == 0:
+                        app.colorSelectIndex = len(app.colors) - 1
+                    else:
+                        app.colorSelectIndex -= 1
+                elif key == 'right':
+                    if app.colorSelectIndex == len(app.colors) - 1:
+                        app.colorSelectIndex = 0
+                    else:
+                        app.colorSelectIndex += 1
+                elif key == 'enter':
+                    app.p1Color = app.colors[app.colorSelectIndex]
+                    app.colors.pop(app.colorSelectIndex)
+                    app.colorSelectIndex = 0
+                    app.p1ColorSelected = True
+            else:
+                if key == 'left':
+                    if app.colorSelectIndex == 0:
+                        app.colorSelectIndex = len(app.colors) - 1
+                    else:
+                        app.colorSelectIndex -= 1
+                elif key == 'right':
+                    if app.colorSelectIndex == len(app.colors) - 1:
+                        app.colorSelectIndex = 0
+                    else:
+                        app.colorSelectIndex += 1
+                elif key == 'enter':
+                    app.p2Color = app.colors[app.colorSelectIndex]
+                    app.colorSelectDone = True
+        elif not app.gridSizeSelectDone:
+            if key == 'right':
+                if app.dim == 10:
+                    app.dim = 4
+                else:
+                    app.dim += 1
+            elif key == 'left':
+                if app.dim == 4:
+                    app.dim = 10
+                else:
+                    app.dim -= 1
+            elif key == 'enter':
+                app.gridSizeSelectDone = True
+        else:
+            if key == 'enter':
+                setActiveScreen('game')
 
 #############################################
 #__gameScreen__
